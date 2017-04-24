162cde5f7cafa3f7798434a9467a81ea530b1eb5
lindseyma <lma2@stuy.edu>
2017-04-24 19:43:24 -0400

    strength

--- passwords.py
+++ passwords.py
@@ -1,6 +1,10 @@
-upper = [[chr(i) for i in range(65,90)], False]
-lower = [[chr(i) for i in range(97,122)], False]
+upper = [[chr(i) for i in range(65,91)], False]
+lower = [[chr(i) for i in range(97,123)], False]
 digits = [[str(i) for i in range(0,10)], False]
+other = [chr(i) for i in range (33,65) if i not in digits]
+
+for i in other:
+	print i
 
 def threshold(password):
 	for i in password:
@@ -16,11 +20,24 @@ def strength(password):
 	ret=1
 	up=0
 	low=0
+	dig=0
+	others=0
 	for i in password:
 		if i in upper[0]:
-			upper+=1
-		ret+=(1/(len(password))
-		for i in low:
-			low+
+			up+=1
+		if i in lower[0]:
+			low+=1
+		if i in digits[0]:
+			dig+=1
+		if i in other:
+			others+=1
+	if up>0 and low>0:
+			ret+=3
+	if dig>0:
+			ret+=3
+	if others>0:
+			ret+=3
+	return ret
 
-print threshold("Hello WorLd")
+
+print strength("hI!0")
